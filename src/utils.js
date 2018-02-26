1dee5d16f25be0d72fd58ad6190c5412d26208b1
Hassan Ali <helfi92@gmail.com>
2018-02-26 16:32:07 -0500

    Enhance the encoding function

--- src/utils.js
+++ src/utils.js
@@ -1,27 +1,15 @@
-// characters that would otherwise be changed when encoded
-const ALLOWED_CHARACTERS_IN_CLIENT_ID = ['|', '@', ':', '+'];
-
 module.exports = {
   // the second capturing group is used to catch a user's github username
   CLIENT_ID_PATTERN: /^([^\/]*\/[^\/]*)\/([^\/]*).*$/,
-  // Limits the encoding so that ALLOWED_CHARACTERS_IN_CLIENT_ID are not encoded
-  // To make sure Client IDs are URL safe, we use a slightly different encoding
-  // Encoding:
-  // 1. Replace ! with !21
-  // 2. Encode with encodeUriComponent (which won't encode !) while making sure allowed characters are not encoded
-  // 3. Replace % with !
-  encode: (str, allowed = ALLOWED_CHARACTERS_IN_CLIENT_ID) => {
-    const encoded = allowed
-      .reduce((acc, curr) => {
-        const encodedSymbol = encodeURIComponent(curr);
+  // Encode manually mainly because encodeUriComponent fails to encode a
+  // few characters that are not legal in a clientId
+  encode: (str) => str
+    .replace(/!/g, '!21')
+    .replace(/[^A-Za-z0-9!@:.+|_-]/g, c => {
+      const h = c.charCodeAt().toString(16).toUpperCase();
 
-        return acc.replace(new RegExp(encodedSymbol, 'g'), curr);
-      }, encodeURIComponent(str.replace(/!/g, '!21')))
-      .replace(/~/g, '!7E')
-      .replace(/%/g, '!');
-
-    return encoded;
-  },
+      return h.length === 2 ? `!${h}` : `!0${h}`;
+    }),
   // Decoding:
   // 1. Replace ! with %
   // 2. Decode with decodeUriComponent (which converts %21 to !)
